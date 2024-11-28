e72d827333589c415e18443589abc5be8d022303
Claude MIAKOUIKILA <claude.miakouikila@tradedoubler.com>
2024-11-28 12:06:11 +0100

    first commit

---
+++ start/routes.ts
@@ -0,0 +1,14 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+import PassionController from "#controllers/passions_controller";
+
+
+router.post("/register", [PassionController, 'handleRequest'] as any);
